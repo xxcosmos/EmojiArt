6ed12d1382b52232c307dd187f785a7b0b5e51aa
xxcosmos <xandcosmos@gmail.com>
2019-10-31 11:44:44 +0800

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by 张啸宇 on 2019/10/31.
+//  Copyright © 2019年 张啸宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
